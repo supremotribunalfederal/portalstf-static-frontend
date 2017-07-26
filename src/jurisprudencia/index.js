354a0e1e272b1a453e26548a239d9f8834a007ce
Lafayette Junio Mendonça Pinheiro <lafayette.m.pinheiro@rede.stf.gov.br>
2017-07-26 16:02:31 -0300

    novo portal 23 - jurisprudencia internacional

--- src/jurisprudencia/index.js
+++ src/jurisprudencia/index.js
@@ -1,1 +1,17 @@
 import style from '../../assets/scss/secoes/jurisprudencia/jurisprudencia.scss';
+
+
+
+//jurisprudencia internacional
+$('#internacional-cjcplp').on('click', function(){
+	ga('send', 'event', 'Jurisprudencia', 'Internacional', 'CJCPLP');
+});
+$('#internacional-codices').on('click', function(){
+	ga('send', 'event', 'Jurisprudencia', 'Internacional', 'Codices');
+});
+$('#internacional-glin').on('click', function(){
+	ga('send', 'event', 'Jurisprudencia', 'Internacional', 'Glin');
+});
+$('#internacional-mercosul').on('click', function(){
+	ga('send', 'event', 'Jurisprudencia', 'Internacional', 'Mercosul');
+})
