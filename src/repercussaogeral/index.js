6460aa8e1ef9be29ffa837fd5c9aac0b7ac0bdc1
Lafayette Junio Mendonça Pinheiro <lafayette.m.pinheiro@rede.stf.gov.br>
2017-07-28 16:54:46 -0300

    novoportal-84 - repercussao geral - quantidade de temas por categoria

--- src/repercussaogeral/index.js
+++ src/repercussaogeral/index.js
@@ -1,1 +1,12 @@
 import style from '../../assets/scss/secoes/repercussaogeral/repercussaogeral.scss';
+
+
+
+//GOOGLE ANALYTICS
+
+$('#repgeral-quantidade-temas-categoria').on('click', function(){
+	ga('send', 'event', 'Repercussão Geral', 'Quantidade de Temas', 'Por Categoria');
+});
+$('#repgeral-quantidade-sobrestados-data').on('click', function(){
+	ga('send', 'event', 'Repercussão Geral', 'Quantidade de Sobrestados', 'Por Data');
+});
