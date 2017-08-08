bbdf2817dc2f67d14696875d5c95e826c79b115f
tsmgodoi <tomas.godoi@stf.jus.br>
2017-08-08 08:34:03 -0300

    Incluindo o datepicker.

--- src/listagem/index.js
+++ src/listagem/index.js
@@ -1,1 +1,21 @@
 import style from '../../assets/scss/secoes/listagem/listagem.scss';
+
+import $ from 'jquery';
+//import 'jquery-ui/themes/base/core.css';
+import 'jquery-ui/ui/widgets/datepicker';
+
+$('#data-inicial').datepicker({
+    dateFormat: "dd/mm/yy"
+});
+$('#data-final').datepicker({
+    dateFormat: "dd/mm/yy"
+});
+
+
+$('#btn-datepicker-inicial').click(function() {
+    $('#data-inicial').datepicker('show');
+});
+
+$('#btn-datepicker-final').click(function() {
+    $('#data-final').datepicker('show');
+});
