b77da2c9beedd0b700c6f616a610490a58573334
Lafayette Junio Mendonça Pinheiro <lafayette.m.pinheiro@rede.stf.gov.br>
2017-08-22 17:37:49 -0300

    novoportal 149 - ostf - transparencia

--- src/ostf/index.js
+++ src/ostf/index.js
@@ -2,3 +2,20 @@ import style from '../../assets/scss/secoes/ostf/ostf.scss';
 import $ from 'jquery';
 
 //GOOGLE ANALYTICS
+
+//card lateral transparencia
+$('#ostf-gestao-pessoas').on('click', function(){
+	ga('send', 'event', 'Página Sobre o STF', 'Transparência', 'Gestão de Pessoas');
+});
+$('#ostf-licitacoes').on('click', function(){
+	ga('send', 'event', 'Página Sobre o STF', 'Transparência', 'Licitações');
+});
+$('#ostf-orcamento').on('click', function(){
+	ga('send', 'event', 'Página Sobre o STF', 'Transparência', 'Orçamento e Finanças');
+});
+$('#ostf-sobrestf').on('click', function(){
+	ga('send', 'event', 'Página Sobre o STF', 'Transparência', 'Conheça mais sobre o STF');
+});
+$('#ostf-vermais-transparencia').on('click', function(){
+	ga('send', 'event', 'Página Sobre o STF', 'Transparência', 'Acessar portal transparência');
+});
