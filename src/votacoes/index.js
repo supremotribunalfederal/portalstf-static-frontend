70ede83cc978b45946c6b69e1b78cbc12edbf784
Lafayette Junio Mendonça Pinheiro <lafayette.m.pinheiro@rede.stf.gov.br>
2017-09-05 19:15:00 -0300

    novoportal 161 - rep geral - ver quadro de votos

--- src/votacoes/index.js
+++ src/votacoes/index.js
@@ -1,23 +1,3 @@
 import style from '../../assets/scss/secoes/votacoes/votacoes.scss';
 import $ from 'jquery';
-
 import moment from 'moment';
-
-$("#sugerir-correcao-texto").click(function() {
-    $("#form-sugerir-correcao-texto").show();
-});
-
-$("#enviar-correcao-texto").click(function() {
-    $("#form-sugerir-correcao-texto").hide();
-    var textoCorrecao = $("#correcao-texto").val();
-
-     // TODO Implementar chamada para o backend.
-    console.log("Enviando correção...");
-    console.log(textoCorrecao);
-
-    $("#correcao-texto").val("");
-});
-
-var dataAtualizacao = $(".data-atualizacao").text();
-var dataMoment = moment(dataAtualizacao);
-$(".data-atualizacao").text(dataMoment.format("dddd, DD") + " de " + dataMoment.format("MMMM").toLowerCase() + " de " + dataMoment.format("YYYY"));
