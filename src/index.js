cb30723b69cd92a6b9ffbb56e1641e7cf1050264
Tulio Macedo <tuliomac@gmail.com>
2017-09-13 18:29:14 -0300

    inclusão de aba para pesquisa de repercussão geral e selecionar a pesquisa correspondente por URL

--- src/index.js
+++ src/index.js
@@ -82,11 +82,17 @@ $("#menuPesquisa li span").on("click", function() {
             $('.pesquisa-noticias-e-textos').show();
             campoInputPesquisa = 'pesquisaNoticiasTextos';
             break;
-        default:
+        case "abaTransparencia":
             placeholder = "Informe o assunto desejado...";
             $("#abaSelecionada").val("6");
             $('.pesquisa-transparencia').show();
             campoInputPesquisa = 'pesquisaTransparencia';
+            break;
+        default:
+            placeholder = "Digite um tema para pesquisar...";
+            $("#abaSelecionada").val("7");
+            $('.pesquisa-repercussao').show();
+            campoInputPesquisa = 'pesquisaRepercussao';
             break;
     }
     
@@ -132,12 +138,18 @@ $("#menu-pesquisa-mobile").on("change", function() {
             $('.pesquisa-noticias-e-textos').show();
             campoInputPesquisa = 'pesquisaNoticiasTextos';
             break;
-        default:
+        case "abaTransparencia":
             placeholder = "Informe o assunto desejado...";
             $("#abaSelecionada").val("6");
             $('.pesquisa-transparencia').show();
             campoInputPesquisa = 'pesquisaTransparencia';
             break;
+        default:
+            placeholder = "Digite um tema para pesquisar...";
+            $("#abaSelecionada").val("7");
+            $('.pesquisa-repercussao').show();
+            campoInputPesquisa = 'pesquisaRepercussao';
+            break;    
     }
     
     $("#pesquisaPrincipal").attr("placeholder", placeholder);
@@ -215,7 +227,10 @@ function realizarPesquisa(id){
         window.open("//stf.jus.br/portal/jurisprudencia/listarConsolidada.asp?base=baseAcordaos&base=baseRepercussao&url=&txtPesquisaLivre=" + encodeURIComponent(termoPesquisa), '_blank');
     } else if (assunto == "3") { // Processo
         pesquisarProcesso();
-    } else {
+    } else if (assunto == "7") { // Repercussão Geral
+        window.open("//stf.jus.br/portal/jurisprudenciaRepercussao/listarProcesso.asp?PesquisaEm=tema&PesquisaEm=controversia&PesquisaEm=ambos&situacaoRG=TODAS&situacaoAtual=S&txtTituloTema=" + encodeURIComponent(termoPesquisa) + "&numeroTemaInicial=&numeroTemaFinal=&acao=pesquisarProcesso&dataInicialJulgPV=&dataFinalJulgPV=&classeProcesso=&numeroProcesso=&ministro=&ordenacao=asc&botao=", '_blank');
+    }
+     else {
         window.open("//stf.jus.br/portal/pesquisa/listarPesquisa.asp?termo=" + encodeURIComponent(termoPesquisa) + "&assunto=" + encodeURIComponent(assunto), '_blank');
     }
 }
@@ -404,4 +419,24 @@ $('#btnAcessarPortalNocicitas').on('click', function() {
     location.href = window.location.origin + window.location.pathname + 'listagem/' + pagina;
 });
 
+// Selecionar a pesquisa por URL
+
+(function PesquisaPorURL() {
+    var url = window.location.pathname;
+    if (url == '/jurisprudencia') {
+        $('#abaJurisprudencia').click();
+    } else if (url == '/noticias' || url == '/textos' ) {
+        $('#abaNoticias').click();
+    } else if (url == '/transparencia') {
+        $('#abaTransparencia').click();
+    } else if (url == '/repercussaogeral') {
+        $('#abaRepercussao').click();
+    } else  {
+        $('#abaProcesso').click();
+    }
+})();
+
+
+
 /* ---------------------------------------------------------------------------------------------------------------- */
+
