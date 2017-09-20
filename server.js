0c1bd8d105d92e32fde8bbebfba304849e50e39b
Tulio Macedo <tuliomac@gmail.com>
2017-09-20 19:36:06 -0300

    Criando uma página de erro 404

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname, '/dist')));
 
+// var uri = url.parse(request.url).pathname
+//   , filename = path.join(process.cwd(), uri);
+//
+// path.exists(filename, function(exists) {
+//     if(!exists) {
+//       response.writeHead(404, {"Content-Type": "text/plain"});
+//       response.write("404 Not Found\n");
+//       response.end();
+//       return;
+//     }
+
 app.listen(3000, function() {
   console.log('Listening from http://localhost:3000');
 })
---
+++ src/404/index.js
@@ -0,0 +1,6 @@
+import style from '../../assets/scss/secoes/404/404.scss';
+import $ from 'jquery';
+
+// teste de uso do js
+
+var a = 1;
