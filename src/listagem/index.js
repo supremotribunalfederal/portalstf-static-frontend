fb36502e84838a7bce1d1a68192708b59e655fad
Anderson.Araujo <anderson.araujo@stf.jus.br>
2017-11-14 21:47:37 -0200

    Implementação da validação dos campos de pesquisa da página de notícias

--- src/listagem/index.js
+++ src/listagem/index.js
@@ -3,10 +3,18 @@ import style from '../../assets/scss/secoes/listagem/listagem.scss';
 import $ from 'jquery';
 import moment from 'moment';
 import URI from 'urijs';
+import validation from 'jquery-validation';
 
 import 'jquery-ui/themes/base/core.css';
 import 'jquery-ui/themes/base/theme.css';
 import 'jquery-ui/themes/base/datepicker.css';
+
+
+$(document).ready(function(){
+    $.validator.addMethod('dataValida', function (value, element) {
+        return this.optional(element) || moment(value,"DD/MM/YYYY").isValid();
+    }, "Data inválida.");
+});
 
 $('#data-inicial').datepicker({
     dateFormat: "dd/mm/yy"
@@ -14,7 +22,6 @@ $('#data-inicial').datepicker({
 $('#data-final').datepicker({
     dateFormat: "dd/mm/yy"
 });
-
 
 $('#btn-datepicker-inicial').click(function() {
     $('#data-inicial').datepicker('show');
@@ -25,19 +32,12 @@ $('#btn-datepicker-final').click(function() {
 });
 
 $('#btn-add-inicial').click(function() {
-    var dataInicial = $('#data-inicial').datepicker('getDate');
-    if (dataInicial) {
-        var dataInicialFormatada = moment(dataInicial).format("DD/MM/YYYY");
-        $('#data-inicial').datepicker('setDate');
-        var url = new URI(window.location.href);
-        url.removeQuery("dataDe");
-        url.addQuery("dataDe", dataInicialFormatada);
-        url.removeQuery("paginaAtual");
-        window.location.href = url.toString();
-    }
+    $('#data-inicial').val('');
+    $('#data-inicial').focus();
 });
 
 $('#btn-add-final').click(function() {
+    /*
     var dataFinal = $('#data-final').datepicker('getDate');
     if (dataFinal) {
         var dataFinalFormatada = moment(dataFinal).format("DD/MM/YYYY");
@@ -48,8 +48,66 @@ $('#btn-add-final').click(function() {
         url.removeQuery("paginaAtual");
         window.location.href = url.toString();
     }
+    */
+    $('#data-final').val('');
+    $('#data-final').focus();
 });
 
+$('#form-pesquisa-noticias').submit(function(e){
+    $('#form-pesquisa-noticias').validate({
+        errorPlacement: function(label, element) {
+            label.addClass('alert alert-danger col-md-12 m-t-16 m-b-0');
+            label.insertAfter('#btnPesquisarNoticias');
+        },
+        wrapper: 'span',
+        rules: {
+            data_inicial: {
+                dataValida: true
+            },
+            data_final: {
+                dataValida: true
+            }
+        },
+        messages: {
+            data_inicial: {
+                dataValida: "Data inicial inválida."
+            },
+            data_final: {
+                dataValida: "Data final inválida."
+            }
+        }
+    });
+
+    if ($('#form-pesquisa-noticias').valid()){
+        console.log('form valido.');
+    }
+});
+/*
+$('#btnPesquisarNoticias').click(function(){
+    var dataInicial = $('#data-inicial').datepicker('getDate');
+    var dataFinal = $('#data-final').datepicker('getDate');
+    var url = new URI(window.location.href);
+    url.removeQuery('dataDe');
+    url.removeQuery('dataA');
+    
+    if (dataInicial){
+        var dataInicialFormatada = moment(dataInicial).format("DD/MM/YYYY");
+        $('#data-inicial').datepicker('setDate');
+        url.removeQuery("dataDe");
+        url.addQuery("dataDe", dataInicialFormatada);
+    }
+
+    if (dataFinal){
+        var dataFinalFormatada = moment(dataFinal).format("DD/MM/YYYY");
+        $('#data-final').datepicker('setDate');
+        url.removeQuery("dataA");
+        url.addQuery("dataA", dataFinalFormatada);
+    }
+
+    url.removeQuery("paginaAtual");
+    window.location.href = url.toString(); 
+});
+*/
 $('.paginacao-ir-para-pagina').click(function () {
     var pagina = parseInt($(this).attr("data-pagina"));
     var url = new URI(window.location.href);
