a7a0156cbc51c3ad6bd272f529f8aff9eb963cdd
Nathália Assis <nathaliamassis@gmail.com>
2017-11-16 17:18:08 -0200

    Select para menu mobile, link para exibir/ocultar informações

--- src/pesquisajurisprudencia/index.js
+++ src/pesquisajurisprudencia/index.js
@@ -16,3 +16,45 @@ filtros.forEach(function(badge){
 $("#apagar_filtro").click(function(){
     $(".badge").empty();
 });
+
+// mostrar/esconder informações completas de pesquisas
+$(function () {
+    
+        $(".informacao-completa").hide();
+        $(".exibir-menos").hide();
+
+        //clicar em exibir mais
+        $(".exibir-mais").bind("click", function (e) {
+            e.preventDefault();                 //não recarregar página ao clicar
+    
+         $(".bloco-right").hide();          
+          if ($(this).attr("class") == "exibir-mais")
+          {
+            $(".informacao-completa").show();
+            $(".exibir-menos").show();
+          }
+          else 
+          { 
+            $(".resumo").show();
+          }
+
+        });
+
+        //clicar em exibir menos
+        $(".exibir-menos").bind("click", function(e){
+            e.preventDefault();     
+
+          $(".informacao-completa").hide();              
+
+          if ($(this).attr("class") == "exibir-menos")
+          {
+            $(".bloco-right").show();
+            $(".exibir-mais").show();
+          }
+          else 
+          { 
+            $(".informacao-completa").show();
+          }
+        });
+    
+    });
