ee31a0bae1596616043015d0153b0883d1ee13ca
Nathália Assis <nathaliamassis@gmail.com>
2017-11-28 17:17:12 -0200

    função para adicionar filtros em todos os botões e apagar, ajustes css e html

--- src/pesquisajurisprudencia/index.js
+++ src/pesquisajurisprudencia/index.js
@@ -2,12 +2,27 @@ import style from '../../assets/scss/secoes/pesquisajurisprudencia/pesquisajuris
 import $ from 'jquery';
 import moment from 'moment';
 
-//excluir filtro aplicado
-
+var divFiltros = document.querySelector("#filtros-aplicados");
 var filtros = document.querySelectorAll(".badge");
+var itens_pesquisados = document.querySelectorAll(".item_pesquisa");
 
+//excluir filtro aplicado 
+divFiltros.addEventListener("click", function(event){
+    event.preventDefault();
+    event.target.remove();
+});
+
+/*
+//excluir filtro aplicado
 filtros.forEach(function(badge){
-    badge.addEventListener("click", function(){
+  badge.addEventListener("click", function(){
+       this.remove();
+   });
+});*/
+
+//excluir itens de pesquisa 
+itens_pesquisados.forEach(function(item_pesquisa){
+    item_pesquisa.addEventListener("click", function(){
         this.remove();
     });
 });
@@ -17,44 +32,46 @@ $("#apagar_filtro").click(function(){
     $(".badge").empty();
 });
 
-// mostrar/esconder informações completas de pesquisas
-$(function () {
-    
-        $(".informacao-completa").hide();
-        $(".exibir-menos").hide();
 
-        //clicar em exibir mais
-        $(".exibir-mais").bind("click", function (e) {
-            e.preventDefault();                 //não recarregar página ao clicar
-    
-         $(".bloco-right").hide();          
-          if ($(this).attr("class") == "exibir-mais")
-          {
-            $(".informacao-completa").show();
-            $(".exibir-menos").show();
-          }
-          else 
-          { 
-            $(".resumo").show();
-          }
-
-        });
-
-        //clicar em exibir menos
-        $(".exibir-menos").bind("click", function(e){
-            e.preventDefault();     
-
-          $(".informacao-completa").hide();              
-
-          if ($(this).attr("class") == "exibir-menos")
-          {
-            $(".bloco-right").show();
-            $(".exibir-mais").show();
-          }
-          else 
-          { 
-            $(".informacao-completa").show();
-          }
-        });
-    
+$(function  (){
+  
+    $(".exibir-mais").on("click", function (e) {
+        e.preventDefault();
+        var blocoParaEsconder = $(this).data('resultado-pesquisa');            
+       $("#"+blocoParaEsconder).hide();   
     });
+
+    //exibir menos
+    $(".exibir-menos").on("click", function (e) {
+        e.preventDefault();
+        var blocoParaEsconder = $(this).data('resultado-pesquisa'); 
+        $("#"+blocoParaEsconder).show(); 
+    });
+ 
+});
+
+
+//adicionar palavras-chaves a pesquisa clicando no botao
+var adicionarFiltro = document.querySelector("#btnAddFiltro");
+adicionarFiltro.addEventListener("click", function(event){
+    event.preventDefault();  
+
+    var formFiltro = document.querySelector("#filtro-palavra-chave");
+    var palavraFiltro = document.querySelector(".palavraPesquisa").value;
+    var span = document.createElement("span");
+    
+    //limpar input
+    $(".palavraPesquisa").val(" ");
+    //criando icone 
+    $(icon).addClass('glyphicon glyphicon-remove-sign');
+    var icon = document.createElement("i");
+    
+    //criando span e conteudo dentro dele
+    span.classList.add("badge");
+    span.textContent = palavraFiltro;
+    span.appendChild(icon);
+    
+    // Adicionando span na div
+    divFiltros.appendChild(span);
+});
+
