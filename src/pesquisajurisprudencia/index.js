7838511d9f7d4daaf280d4a126d5b21a5568d696
Nathália Assis <nathaliamassis@gmail.com>
2017-12-11 16:29:34 -0200

    radio buttons para pesquisa completa/resumida

--- src/pesquisajurisprudencia/index.js
+++ src/pesquisajurisprudencia/index.js
@@ -7,8 +7,12 @@ var filtros = document.querySelectorAll(".badge");
 var itens_pesquisados = document.querySelectorAll(".item_pesquisa");
 var removeFiltro = document.querySelectorAll(".remover");
 
+$(document).ready(function(){
+    padraoPesquisa();
+});
+
 //excluir filtro aplicado
-$(divFiltros).click(function(event){
+$(divFiltros).dblclick(function(event){
     event.preventDefault();
     event.target.closest('.badge').remove();
 });
@@ -25,8 +29,7 @@ $("#apagar_filtro").click(function(){
     $(".badge").empty();
 });
 
-
-$(function  (){
+function botoesExibir(){
     $(".exibir-mais").on("click", function (e) {
         e.preventDefault();
         var blocoParaEsconder = $(this).data('resultado-pesquisa');            
@@ -43,8 +46,24 @@ $(function  (){
     $(".outras-infos").on("click", function(e){
         e.preventDefault();
     });
-});
+}
+   
 
+
+function criarBadge(valorFiltro, classe){
+    var span = document.createElement("span");
+    var icon = document.createElement("i");
+    var texto = document.createElement("text");
+
+    //criando span e conteudo dentro do span
+    span.appendChild(texto);
+    texto.textContent = valorFiltro;
+    span.appendChild(icon);
+    span.classList.add(classe, "badge");
+    $(icon).addClass('glyphicon glyphicon-remove-sign');
+
+    divFiltros.appendChild(span);  
+};
 
 //função  para adicionar filtro
 $(function(){
@@ -53,28 +72,66 @@ $(function(){
 
         var idDoCampo = $(this).data('add-filtro');
         var classe = $(this).data('classe-campo');
-        var valorFiltro = $('#'+idDoCampo).val();
-        var span = document.createElement("span");
-        var icon = document.createElement("i");
-        var text = document.createElement("text");
-      
-        //criando span e conteudo dentro do span
-        span.appendChild(text);
-        text.textContent = valorFiltro;
-        span.appendChild(icon);
-        span.classList.add(classe, "badge");
-        $(icon).addClass('glyphicon glyphicon-remove-sign');
-        console.log(span);
-        
-        // efetua ação apenas se houver conteudo no input
-        if($('#'+idDoCampo).val().length != 0){
-            // Adicionando span na div
-            divFiltros.appendChild(span);  
-            //limpar input
-            $('#'+idDoCampo).val("");    
-        } 
+        var valorFiltro = $('#'+idDoCampo).val().split(";");
+
+        valorFiltro.forEach(function(valor){
+            var tirarEspaco = $.trim(valor);
+
+            if( tirarEspaco != ""){
+                criarBadge(tirarEspaco, classe);
+            }
+            
+        });
+        $('#'+idDoCampo).val(""); 
+
     });
 });
+//trazer lista de pesquisas abertas ou fechadas
+var pesquisaCompleta = $(".btn-completa");
+var pesquisaResumida = $(".btn-resumida");
+var infoCompleta = $(".informacao-completa");
+
+$(pesquisaCompleta).attr("checked", true);
+
+function padraoPesquisa(){
+    if($(pesquisaCompleta).attr("checked") == "checked"){
+        $(".bloco-right").hide();
+        $(".exibir-menos").hide();
+        $(".outras-infos").removeClass('collapse');
+        $(".exibirMaisInformacoes").hide();                 
+    }
+}; 
+
+$(pesquisaCompleta).on("click",function(){
+    pesquisaResumida.attr("checked", false);
+    pesquisaCompleta.attr("checked", true);
+    $(infoCompleta).removeClass('collapse'); 
+    padraoPesquisa();
+});
+    
+$(pesquisaResumida).on("click", function(){ 
+    pesquisaCompleta.attr("checked", false);
+    pesquisaResumida.attr("checked", true);
+    $(infoCompleta).addClass('collapse');
+    $(".outras-infos").addClass('collapse');
+    $(".bloco-right").show();
+    $(".exibirMaisInformacoes").show();
+    $(".exibir-menos").show();
+    botoesExibir();    
+});
+
+
+
+ 
+
+
+
+
+
+
+
+
+
 
 
 
