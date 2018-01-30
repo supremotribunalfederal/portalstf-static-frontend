1c7304739aa74aa13f21d7955f6110b2ddacced1
ElmarRoberto <elmarcfilho@gmail.com>
2018-01-29 22:35:01 -0200

    [Refatoração] Comentário de métodos e mudança de nome de variáveis

--- src/pesquisaavancada/index.js
+++ src/pesquisaavancada/index.js
@@ -2,19 +2,6 @@ import style from '../../assets/scss/secoes/pesquisaavancada/pesquisaavancada.sc
 import $ from 'jquery';
 import moment from 'moment';
 
-/*
-$(document).ready(function() {
-	$("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
-			e.preventDefault();
-			$(this).siblings('a.active').removeClass("active");
-			$(this).addClass("active");
-			var index = $(this).index();
-			$("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
-			$("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
-	});
-});
-*/
-
 //mostrar e esconder icone descricao do tema
 var mostrar=$('.more');
 var esconder = $('.less');
@@ -32,24 +19,28 @@ esconder.click(function(e){
 });
 
 /**
- * @description 
+ * @description Filtra dados dentro algum elemento(tabela, lista, etc).
+ * @params {String} dadoDeEntrada string vinda da entrada do usuário.
+ * @params {String} elementoASerfiltrado id ou class do elemento que possui dados para filtragem.
+ * @params {String} tagParaFiltragem tag principal que se deseja filtrar os dados do elemento.
  */
-var pesquisa = function(input, fonte, tipo) {
+var filtraDados = function(dadoDeEntrada, elementoASerfiltrado, tagParaFiltragem) {
 	$(document).ready(function(){
-		$(input).on("keyup", function() {
-			var value = $(this).val().toLowerCase();
-			$(fonte + " " + tipo).filter(function() {
-				$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		$(dadoDeEntrada).on("keyup", function() {
+			var valorCapturado = $(this).val().toLowerCase();
+			$(elementoASerfiltrado + " " + tagParaFiltragem).filter(function() {
+				$(this).toggle($(this).text().toLowerCase().indexOf(valorCapturado) > -1)
 			});
 		});
 	});
 }
 
-pesquisa("#input-pesquisa-livre", "#tabela-RG", "tr");
-pesquisa("#input-busca-ministro", ".dropdown-menu-ministros", "li");
+filtraDados("#input-pesquisa-livre", "#tabela-RG", "tr");
+filtraDados("#input-busca-ministro", ".dropdown-menu-ministros", "li");
 
-// codigos teste
-
+/**
+ * @description Alterna os estados de ativo de cada aba da tab de navegação dos filtros
+*/
 $(document).ready(function() {
     $("div.list-group>a").click(function(e) {
         e.preventDefault();
