12cbbae3ad6f92498684821d176a4882498bc43d
Nathália Assis <nathaliamassis@gmail.com>
2018-02-23 17:28:52 -0300

    modal plenário virtual

--- src/pesquisaavancada/index.js
+++ src/pesquisaavancada/index.js
@@ -2,22 +2,6 @@ import style from '../../assets/scss/secoes/pesquisaavancada/pesquisaavancada.sc
 import $ from 'jquery';
 import moment from 'moment';
 
-//mostrar e esconder icone descricao do tema
-var mostrar=$('.more');
-var esconder = $('.less');
-
-mostrar.click(function(e){
-	e.preventDefault();
-	mostrar.hide();
-	esconder.show();
-});
-
-esconder.click(function(e){
-	e.preventDefault();
-	esconder.hide();
-	mostrar.show();
-});
-
 /**
  * @description Filtra dados dentro algum elemento(tabela, lista, etc).
  * @params {String} dadoDeEntrada string vinda da entrada do usuário.
@@ -82,7 +66,7 @@ $(document).ready(function() {
 });
 
 /**
- * @description Limpa filtros
+ * @description Limpar filtros
  */
 $(document).ready(function() {
 	$(".limpar-filtros").click(function() {
@@ -95,14 +79,30 @@ $(document).ready(function() {
 
 
 // esconder link pesquisa-avançada 
-	$('.link-pesquisa-avancada-rg').click(function(){
+	$('.link-pesquisa-avancada-rg').click(function(e){
+		e.preventDefault();
 		$(this).hide();
 	});
-	$('.esconder-filtros').click(function(){
+	$('.esconder-filtros').click(function(e){
+		e.preventDefault();
 		$('.link-pesquisa-avancada-rg').show();
 	});
 
+// abrir modal plenario
 
+$(".progress").click(function(){
+	$('#li-detalhes').removeClass('active');
+	$('#li-votos').addClass('active');
+	$('#tab-detalhes').removeClass('active in');
+	$('#tab-votos').addClass('active in');
+});
+
+$(".num_tema").click(function(){
+	$('#li-detalhes').addClass('active');
+	$('#li-votos').removeClass('active');
+	$('#tab-detalhes').addClass('active in');
+	$('#tab-votos').removeClass('active in');
+});
 
 // mudar nome da pesquisa de acordo com o filtro selecionado
 
