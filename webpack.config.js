346d106d44c02a6b12184aa256b6b21fbe80a6b9
cacio-costa <cacio.cjcs@gmail.com>
2018-04-26 13:35:17 -0300

    parametriza ID dos objetos QLIK (repercussão geral)

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,10 @@ let QLIK = {
         ID: JSON.stringify('2f46c444-d61d-48f9-b6b3-124773a3c764')
     },
     REPERCUSSAOGERAL: {
-        ID: JSON.stringify('b88460cb-8745-4fbc-973b-ddfee50c02aa')
+        ID: JSON.stringify('c1de5fc6-2619-4161-a007-0d078fdc1d82'),
+        TABELA_SITUACAO_RG: JSON.stringify('EbEjsXW'),
+        TABELA_SITUACAO_MERITO: JSON.stringify('BUbLbb'),
+        TABELA_MERITOS_JULGADOS_ANO: JSON.stringify('HmJAy')
     }
 };
 
@@ -56,7 +59,10 @@ if (process.env.NODE_ENV == 'production') {
             ID: JSON.stringify('9e787043-f90c-4781-a48b-c1351ba17379')
         },
         REPERCUSSAOGERAL: {
-            ID: JSON.stringify('b88460cb-8745-4fbc-973b-ddfee50c02aa')
+            ID: JSON.stringify('b88460cb-8745-4fbc-973b-ddfee50c02aa'),
+            TABELA_SITUACAO_RG: JSON.stringify('NQbAxS'),
+            TABELA_SITUACAO_MERITO: JSON.stringify('b60a6338-5e54-4c85-97bf-f8a6b0424eca'),
+            TABELA_MERITOS_JULGADOS_ANO: JSON.stringify('arPD')
         }
     };
 }
