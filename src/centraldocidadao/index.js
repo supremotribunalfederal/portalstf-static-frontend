c6c474f4c4e7b714bac835b0e7fa1b46d794730d
Nathália Assis <nathaliamassis@gmail.com>
2018-05-02 16:58:49 -0300

    criação parte 1 e 2 de cadastro

--- src/centraldocidadao/index.js
+++ src/centraldocidadao/index.js
@@ -1,3 +1,36 @@
-import style from '../../assets/scss/secoes/centraldocidadao/centraldocidadao/.scss';
+import style from '../../assets/scss/secoes/centraldocidadao/centraldocidadao.scss';
 import $ from 'jquery';
 import moment from 'moment';
+
+//focar no input do tipo de documento selecionado 
+$('#tipoCPF').click(function(){
+    $('.input-cpf').focus();
+    $("#tipoCPF").prop("checked", true);
+    $('#tipoRG').prop("checked", false);
+});
+$('#tipoRG').click(function () {
+    $('.input-rg').focus();
+    $("#tipoCPF").prop("checked", false);
+    $('#tipoRG').prop("checked", true);
+});
+
+//paginação 
+$(function(){
+    $('#parte2').hide();
+//parte 1 
+    $(".btn-next-1").click(function(){
+        $('#parte1').hide();
+        $('#parte2').show();
+    });
+//parte 2
+    $('.btn-voltar-2').click(function(){
+        $('#parte2').hide();
+        $('#parte1').show();
+    });
+
+    $(".btn-next-2").click(function () {
+        $('#parte2').hide();
+        $('#parte3').show();
+      
+    });
+});
